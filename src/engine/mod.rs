e331c7ca6f471e708dcb14643625447352c01ea2
Richard Mapes <richard.mapes@gmail.com>
2025-09-27 13:08:48 +0100

    Allow engine to communicate state back to gui

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -2,33 +2,76 @@ mod synth;
 
 use synth::play_midi;
 use std::error::Error;
-use std::sync::mpsc;
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 
 pub struct EngineController {
     tx: mpsc::Sender<Actions>,
 }
 
+pub struct PlayerState {
+    pub is_playing: bool
+}
+
+pub struct StateObserver<F> 
+where 
+    F: Fn(&PlayerState) + Send + Sync + 'static,
+{
+    on_change: F,
+    player_state: Arc<Mutex<PlayerState>>,
+}
+
+impl<F> StateObserver<F> 
+where 
+    F: Fn(&PlayerState) + Send + Sync + 'static,
+{
+    fn new(callback: F, player_state: Arc<Mutex<PlayerState>>) -> StateObserver<F> {
+        StateObserver {
+            on_change: callback,
+            player_state,
+        }
+    }
+    pub fn notify(&self) {
+        if let Ok(state) = self.player_state.lock() {
+            (self.on_change)(&state);
+        }
+    }
+}
 enum Actions {
     PlayMidi,
     Quit,
 }
 
-pub fn start() -> EngineController {
+pub fn start<F>(observer_callback: F) -> EngineController 
+where 
+    F: Fn(&PlayerState) + Send + Sync + 'static {
     let (tx, rx) = mpsc::channel::<Actions>();
+    let player_state = Arc::new(Mutex::new(PlayerState { is_playing: false }));
+
+    let observer = StateObserver::new(observer_callback, Arc::clone(&player_state));
 
     thread::spawn(move || {
        loop {
         let received = rx.recv().unwrap();
         match received {
-            Actions::PlayMidi => play_scale().unwrap(),
+            Actions::PlayMidi => {
+                if let Ok(mut state) = player_state.lock() {
+                    state.is_playing = true;
+                }
+                observer.notify();
+                play_scale().unwrap();
+                if let Ok(mut state) = player_state.lock() {
+                    state.is_playing = false;
+                }
+                observer.notify();
+            },
             Actions::Quit => break,
         }
        }
     });
 
     
-    return EngineController {tx};
+    return EngineController {tx}
 }
 
 impl EngineController {
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,29 @@ mod engine;
 
 slint::include_modules!();
 
+use engine::PlayerState;
+
 use std::rc::Rc;
 
 
 fn main() -> Result<(), slint::PlatformError> {
     let main_window = MainWindow::new()?;
-    let engine = Rc::new(engine::start());
+    let engine = Rc::new(engine::start({
+        let win = main_window.as_weak();
+        move |player_state: &PlayerState| {
+            let is_playing = player_state.is_playing;
+            let _ = win.upgrade_in_event_loop(
+                move |handle| {
+                    handle.set_is_playing(is_playing)
+                }
+            );
+        }
+    }));
     let engine_clone = Rc::clone(&engine);
     
+    // Handle state updates
+    
+
     // Set up callbacks
     main_window.on_play_midi(move || {
         engine_clone.play_midi();
