9be8db66b7dd5ad7bd17a521bb4a6c682116903a
Richard Mapes <richard.mapes@gmail.com>
2025-11-01 22:45:37 +0000

    Add log consumer for output to console

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,10 @@ mod ui;
 
 
 
+
 fn main() -> Result<(), Box<dyn Error>> {
+    pretty_env_logger::init();
+    log::info!("Starting app");
     ui::run().map_err(|err| Box::new(err) as Box<dyn Error>)
  
     // Handle state updates
